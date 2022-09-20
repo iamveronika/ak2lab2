a491ca01232382b78477875062981e7d54a31a2b
Veronika Kondratenko <97289112+iamveronika@users.noreply.github.com>
2022-09-20 19:59:45 +0300

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,15 @@
+class Calculator
+{
+    public:
+<<<<<<< HEAD
+<<<<<<< HEAD
+        int Add (int, int);
+=======
+        static int Add (int, int);
+        static int Mul (double, double);
+>>>>>>> d61d32d (mark member-function as `static`)
+=======
+        static int Add (int, int);
+        static int Mul (double, double);
+>>>>>>> d61d32d (mark member-function as `static`)
+};
